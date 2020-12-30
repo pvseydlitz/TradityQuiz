13b89a06b301cff11f46cfae6271dedfe2503cc4
Philipp von Seydlitz <pvseydlitz@me.com>
2020-12-30 17:02:18 +0100

    Get questions from google sheets

--- index.js
+++ index.js
@@ -10,15 +10,61 @@ function setQuestions() {
       // SUCCESS
       res
         .json()
-        .then((data) => {
-          const yourData = data;
-          console.log(yourData);
+        .then((fragenDaten) => {
+          console.log(fragenDaten);
+          setHTML(fragenDaten);
         })
         .catch((err) => console.log(err));
     } else {
       // ERROR
     }
   });
+}
+
+function setHTML(fragenDaten) {
+  const form = document.getElementById('quizForm');
+  if (fragenDaten.data[0].Art === 'Multiple-Choice 4 Antworten') {
+    let template = document
+      .getElementById('4RadioButtons')
+      .content.cloneNode(true);
+    const frage = template.querySelector('#frage');
+    frage.innerHTML = fragenDaten.data[0].Frage;
+    const möglichkeit1 = template.querySelector('#möglichkeit1');
+    möglichkeit1.innerHTML = fragenDaten.data[0].Antwortmöglichkeit1;
+    const möglichkeit2 = template.querySelector('#möglichkeit2');
+    möglichkeit2.innerHTML = fragenDaten.data[0].Antwortmöglichkeit2;
+    const möglichkeit3 = template.querySelector('#möglichkeit3');
+    möglichkeit3.innerHTML = fragenDaten.data[0].Antwortmöglichkeit3;
+    const möglichkeit4 = template.querySelector('#möglichkeit4');
+    möglichkeit4.innerHTML = fragenDaten.data[0].Antwortmöglichkeit4;
+    form.appendChild(template);
+  }
+  if (fragenDaten.data[0].Art === 'Multiple-Choice 3 Antworten') {
+    let template = document
+      .getElementById('3RadioButtons')
+      .content.cloneNode(true);
+    const frage = template.querySelector('#frage');
+    frage.innerHTML = fragenDaten.data[0].Frage;
+    const möglichkeit1 = template.querySelector('#möglichkeit1');
+    möglichkeit1.innerHTML = fragenDaten.data[0].Antwortmöglichkeit1;
+    const möglichkeit2 = template.querySelector('#möglichkeit2');
+    möglichkeit2.innerHTML = fragenDaten.data[0].Antwortmöglichkeit2;
+    const möglichkeit3 = template.querySelector('#möglichkeit3');
+    möglichkeit3.innerHTML = fragenDaten.data[0].Antwortmöglichkeit3;
+    form.appendChild(template);
+  }
+  if (fragenDaten.data[0].Art === 'Multiple-Choice 2 Antworten') {
+    let template = document
+      .getElementById('2RadioButtons')
+      .content.cloneNode(true);
+    const frage = template.querySelector('#frage');
+    frage.innerHTML = fragenDaten.data[0].Frage;
+    const möglichkeit1 = template.querySelector('#möglichkeit1');
+    möglichkeit1.innerHTML = fragenDaten.data[0].Antwortmöglichkeit1;
+    const möglichkeit2 = template.querySelector('#möglichkeit2');
+    möglichkeit2.innerHTML = fragenDaten.data[0].Antwortmöglichkeit2;
+    form.appendChild(template);
+  }
 }
 
 function submitData() {
